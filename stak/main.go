3cce79f4ec0b804674f5fc48f4c80177b41b5e5f
RadKirill00 <Radkirill00@mail.ru>
2024-10-29 09:59:15 +0300

    complete stak

--- stak/main.go
+++ stak/main.go
@@ -24,6 +24,27 @@ func s_push(top **Comp, D int) {
 	}
 }
 
+func s_delete_key(top **Comp, N int) {
+ q := *top
+ var prev *Comp
+
+ for q != nil {
+	if q.Data == N {
+		if q == *top {
+			*top = q.Next
+		} else {
+			prev.Next = q.Next
+		}
+		q.Data = 0
+		q.Next = nil
+		return
+	}
+	prev = q
+	q = q.Next
+ }
+}
+
+
 func main() {
 	var top *Comp
 	s_push(&top, 10)
@@ -31,6 +52,13 @@ func main() {
 	s_push(&top, 30)
 
 	// Выводим элементы стека
+	for current := top; current != nil; current = current.Next {
+		fmt.Println(current.Data)
+	}
+
+	s_delete_key(&top, 20)
+
+	fmt.Println("After deletion:")
 	for current := top; current != nil; current = current.Next {
 		fmt.Println(current.Data)
 	}
